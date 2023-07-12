49bf0189ae0a8020cd8113bbbfdd1d59a8e1791e
tiagooliveiragithub <contacto.tiagooliveira@gmail.com>
2023-07-12 19:51:01 +0100

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.tiago.despedidasolteirolda {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+        
+    opens com.tiago.despedidasolteirolda to javafx.fxml;
+    exports com.tiago.despedidasolteirolda;
+}
